1b45c69efecb0dade7fc958bcc1889e714e1b9a7
DonnyLVu <donnyvu897@gmail.com>
2020-10-28 15:03:04 -0700

    add basic file structure for react

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className="footer">HI IM THE FOOTER</h2>
+            </div>
+        )
+    }
+}
